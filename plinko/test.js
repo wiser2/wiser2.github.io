2ff4fb04220496734ea2d0dd2cea4e37a121f43f
justin <justinjlu@gmail.com>
2025-02-09 19:38:47 +1100

    rebalance goals

--- plinko/test.js
+++ plinko/test.js
@@ -14,8 +14,18 @@ var render = Render.create({
     engine: engine
 });
 
+const ball_category = 0x0001
+const peg_category = 0x0002
+const ground_category = 0x0004
+
 var ceiling = Bodies.rectangle(0, -100, 800, 100, { isStatic: true });
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+var ground = Bodies.rectangle(400, 610, 810, 60, { 
+    isStatic: true,
+    collisionFilter: {
+        category: ground_category,
+        mask: ball_category
+    }
+ });
 var peg = Bodies.circle(400, 300, 5, { isStatic: true });
 var ball = Bodies.circle(370 + (Math.random() - 0.5) * 1.5, 75 + (Math.random() - 0.5) * 1.5, 10, { isStatic: false });
 Matter.Body.setInertia(ball, 7)
@@ -30,78 +40,121 @@ var bet = parseFloat(bet_input.value)
 
 var runs = 0;
 
+var balls = []
+
+
+
+class Ball {
+    constructor() {
+        this.finished = false
+        this.frames = 0
+        this.bet = bet_input.value
+        this.score_calculated = false
+
+
+        this.body = Bodies.circle(370 + (Math.random() - 0.5) * 1.5, 75 + (Math.random() - 0.5) * 1.5, 10, {
+            collisionFilter: {
+                category: ball_category,
+                mask: peg_category | ground_category
+            }
+        });
+        Matter.Body.setInertia(this.body, 8)
+        Composite.add(engine.world, this.body);
+
+        score = score - this.bet
+        document.getElementById('score').innerHTML = score
+
+        console.log('making ball')
+        balls.push(this)
+    }
+
+
+}
+
+
+
 
 // make a pyramid of pegs
 for (var i = 0; i < 10; i++) {
     for (var j = 0; j < i; j++) {
-        var peg = Bodies.circle(400 + j * 60 - (i * 30), i * 50 + 75, 7, { isStatic: true });
+        const peg = Bodies.circle(400 + j * 60 - (i * 30), i * 50 + 75, 7, {
+            collisionFilter: {
+                category: peg_category,
+                mask: ball_category
+            }, isStatic: true
+        });
         Composite.add(engine.world, peg);
         if (i == 9) {
-            var wall = Bodies.rectangle(400 + j * 60 - (i * 30), 580, 10, 50, { isStatic: true });
+            var wall = Bodies.rectangle(400 + j * 60 - (i * 30), 580, 10, 50, {
+                isStatic: true,
+                collisionFilter: {
+                    category: ground_category,
+                    mask: ball_category
+                }
+            });
             Composite.add(engine.world, wall);
         }
     }
 }
 
-var left_wall = Bodies.rectangle(70, 580, 10, 50, { isStatic: true });
-var right_wall = Bodies.rectangle(670, 580, 10, 50, { isStatic: true });
+var left_wall = Bodies.rectangle(70, 580, 10, 50, {
+    isStatic: true,
+    collisionFilter: {
+        category: ground_category,
+        mask: ball_category
+    }
+});
+var right_wall = Bodies.rectangle(670, 580, 10, 50, {
+    isStatic: true,
+    collisionFilter: {
+        category: ground_category,
+        mask: ball_category
+    }
+});
 Composite.add(engine.world, [left_wall, right_wall]);
 
+
 function reset_ball() {
-    if (runs == 0) {
-        Composite.add(engine.world, ball);
-    }
-
-    runs = runs + 1
-    finished = false
-    score_calculated = false
-    frames = 0
-
-    bet = parseFloat(bet_input.value)
-    score = parseFloat(score) - bet
-    score = parseFloat(score).toFixed(2)
-    document.getElementById('score').innerHTML = score
-
-    var new_x = 370 + (Math.random() - 0.5) * 1.5
-    var new_y = 75 + (Math.random() - 0.5) * 1.5
-    Matter.Body.translate(ball, { x: new_x - ball.position.x, y: new_y - ball.position.y })
-    Matter.Body.setVelocity(ball, { x: 0, y: 0 })
-
+    ball = new Ball()
 }
 
-function check_for_finish() {
-    if (ball.position.y > 568) {
+function check_for_finish(ball_obj) {
+    if (ball_obj.body.position.y > 566) {
         console.log('at bottom')
-        finished = true
+        ball_obj.finished = true
     }
 }
 
-function check_multiplier() {
+function check_multiplier(ball_obj) {
     var multiplier = 1
-    var x = ball.position.x
-    if (ball.position.y > 568) {
+    var x = ball_obj.body.position.x
+    if (ball_obj.body.position.y > 566) {
         if (320 < x && x < 415) {
-            // console.log('0.1x')s
             multiplier = 0.1
         } else if (260 < x && x < 320 || 425 < x && x < 480) {
-            // console.log('0.6x')
-            multiplier = 0.6
+            multiplier = 0.3
         } else if (200 < x && x < 260 || 480 < x && x < 535) {
-            // console.log('3x')
-            multiplier = 3
+            multiplier = 1
         } else if (140 < x && x < 200 || 545 < x && x < 595) {
-            // console.log('50x')
-            multiplier = 50
+            multiplier = 20
         } else if (80 < x && x < 140 || 605 < x && x < 655) {
-            // console.log('1000x')
-            multiplier = 1000
+            multiplier = 50
         }
     }
+    console.log(multiplier)
     score = parseFloat(score) + bet * multiplier
     score = parseFloat(score).toFixed(2)
     console.log(bet * multiplier)
     document.getElementById('score').innerHTML = score
-    score_calculated = true
+    ball_obj.score_calculated = true
+}
+
+
+function deleteAllBalls() {
+    for (var i = 0; i < balls.length; i++) {
+        Composite.remove(engine.world, balls[i].body)
+    }
+    balls = []
 }
 
 // add all of the bodies to the world
@@ -115,15 +168,26 @@ var finished = false;
 var score_calculated = false;
 // create runner
 (function run() {
-    frames += 1
+    // frames += 1
+
+    for (var i = 0; i < balls.length; i++) {
+        balls[i].frames += 1
+    }
     window.requestAnimationFrame(run);
     Engine.update(engine, 1000 / 60);
 
-    if (frames > 100 && !finished) {
-        check_for_finish()
-    } else if (finished && !score_calculated) {
-        check_multiplier()
+    for (var i = 0; i < balls.length; i++) {
+        if (balls[i].frames > 100 && !balls[i].finished) {
+            check_for_finish(balls[i])
+        } else if (balls[i].finished && !balls[i].score_calculated) {
+            check_multiplier(balls[i])
+        }
     }
+    // if (frames > 100 && !finished) {
+    //     check_for_finish()
+    // } else if (finished && !score_calculated) {
+    //     check_multiplier()
+    // }
     
 
 })();
